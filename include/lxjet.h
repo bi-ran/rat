cb6bf4c68688abb15eae02852b00da126b6ac870
Ran Bi <biran@mit.edu>
2018-10-08 21:55:18 -0400

    update trigger list
    /users/rbi/scratch/egxtriggersfrominna/V35

--- include/lxjet.h
+++ include/lxjet.h
@@ -23,6 +23,18 @@
    ACTION(HLT_HIL2Mu7Eta2p5_PuAK4CaloJet60Eta2p1_v1, ## __VA_ARGS__) \
    ACTION(HLT_HIL2Mu7Eta2p5_PuAK4CaloJet80Eta2p1_v1, ## __VA_ARGS__) \
    ACTION(HLT_HIL2Mu7Eta2p5_PuAK4CaloJet100Eta2p1_v1, ## __VA_ARGS__)\
+   ACTION(HLT_HIL3Mu3Eta2p5_PuAK4CaloJet40Eta2p1_v1, ## __VA_ARGS__) \
+   ACTION(HLT_HIL3Mu3Eta2p5_PuAK4CaloJet60Eta2p1_v1, ## __VA_ARGS__) \
+   ACTION(HLT_HIL3Mu3Eta2p5_PuAK4CaloJet80Eta2p1_v1, ## __VA_ARGS__) \
+   ACTION(HLT_HIL3Mu3Eta2p5_PuAK4CaloJet100Eta2p1_v1, ## __VA_ARGS__)\
+   ACTION(HLT_HIL3Mu5Eta2p5_PuAK4CaloJet40Eta2p1_v1, ## __VA_ARGS__) \
+   ACTION(HLT_HIL3Mu5Eta2p5_PuAK4CaloJet60Eta2p1_v1, ## __VA_ARGS__) \
+   ACTION(HLT_HIL3Mu5Eta2p5_PuAK4CaloJet80Eta2p1_v1, ## __VA_ARGS__) \
+   ACTION(HLT_HIL3Mu5Eta2p5_PuAK4CaloJet100Eta2p1_v1, ## __VA_ARGS__)\
+   ACTION(HLT_HIL3Mu7Eta2p5_PuAK4CaloJet40Eta2p1_v1, ## __VA_ARGS__) \
+   ACTION(HLT_HIL3Mu7Eta2p5_PuAK4CaloJet60Eta2p1_v1, ## __VA_ARGS__) \
+   ACTION(HLT_HIL3Mu7Eta2p5_PuAK4CaloJet80Eta2p1_v1, ## __VA_ARGS__) \
+   ACTION(HLT_HIL3Mu7Eta2p5_PuAK4CaloJet100Eta2p1_v1, ## __VA_ARGS__)\
    ACTION(HLT_HIEle10Gsf_PuAK4CaloJet40Eta2p1_v1, ## __VA_ARGS__)    \
    ACTION(HLT_HIEle10Gsf_PuAK4CaloJet60Eta2p1_v1, ## __VA_ARGS__)    \
    ACTION(HLT_HIEle10Gsf_PuAK4CaloJet80Eta2p1_v1, ## __VA_ARGS__)    \
